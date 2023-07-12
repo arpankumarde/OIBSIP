69039c1754887792df02483caf20481d566924c4
arpankumarde <academiaindiana@outlook.com>
2023-07-12 23:49:20 +0530

    added task entry point and state holding the data

--- react-todo-list/src/components/TaskEntry.jsx
+++ react-todo-list/src/components/TaskEntry.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const TaskEntry = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // console.log("in here")
+    // console.log(inputs)
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // alert(inputs);
+    console.log(inputs);
+    setTasks(prev => [...prev, inputs]);
+    setInputs({});
+    console.log(tasks);
+  }
+
   return (
-    <div>
-      TaskEntry
+    <div className='bg-gray-300 w-1/3 flex justify-center items-center'>
+      <div className="bg-yellow-300 p-4 rounded-lg">
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <fieldset className='flex flex-col'>
+            <span>Title</span>
+            <input type="text" name='title'
+              value={inputs.title || ""}
+              onChange={handleChange}
+              placeholder='Enter a title'
+              required
+            />
+          </fieldset>
+          <fieldset className='flex flex-col'>
+            <span>Description</span>
+            <textarea name='desc' value={inputs.desc || ""}
+              onChange={handleChange}
+              placeholder='Enter Task description'
+              className='w-full'
+            >
+            </textarea>
+          </fieldset>
+          <button type="submit">Add to list</button>
+        </form>
+      </div>
     </div>
   )
 }
--- react-todo-list/src/components/TaskList.jsx
+++ react-todo-list/src/components/TaskList.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div>TaskList</div>
+    <section className='bg-pink-200 w-2/3 flex items-center justify-center px-20'>
+      <div className="container bg-slate-400 min-h-[40vh]">
+        TaskList
+      </div>
+    </section>
   )
 }
 
