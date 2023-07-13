416012be4b87ce3271f36dbb9525eaca5c592112
arpankumarde <academiaindiana@outlook.com>
2023-07-13 22:58:11 +0530

    added mark complete/incomplete functionality

--- react-todo-list/src/components/TaskEntry.jsx
+++ react-todo-list/src/components/TaskEntry.jsx
@@ -4,7 +4,7 @@ const TaskEntry = ({ tasks, setTasks }) => {
   const [inputs, setInputs] = useState({});
 
   const handleChange = (event) => {
-    const name = event.target.name;
+    const name = event.target.name.replace(/\s{2,}/g, ' ').trim();
     const value = event.target.value;
     // console.log("in here")
     // console.log(inputs)
@@ -23,27 +23,29 @@ const TaskEntry = ({ tasks, setTasks }) => {
 
   return (
     <div className='bg-gray-300 w-1/3 flex justify-center items-center'>
-      <div className="bg-yellow-300 p-4 rounded-lg">
-        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      <div className="bg-yellow-300 p-4 rounded-lg w-2/3">
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4' autoComplete='disabled'>
           <fieldset className='flex flex-col'>
             <span>Title</span>
             <input type="text" name='title'
-              value={inputs.title || ""}
-              onChange={handleChange}
+              value={inputs.title || ""} onChange={handleChange}
               placeholder='Enter a title'
-              required
+              autoComplete='disabled'
+              className='w-full px-2 py-1'
+              autoFocus required
             />
           </fieldset>
           <fieldset className='flex flex-col'>
             <span>Description</span>
-            <textarea name='desc' value={inputs.desc || ""}
-              onChange={handleChange}
+            <textarea type='text' name='desc'
+              value={inputs.desc || ""} onChange={handleChange}
               placeholder='Enter Task description'
-              className='w-full'
+              className='w-full p-2 h-36 resize-none'
+              autoComplete='disabled'
             >
             </textarea>
           </fieldset>
-          <button type="submit">Add to list</button>
+          <button type="submit" className='bg-green-300 rounded-md p-1'>Add to list</button>
         </form>
       </div>
     </div>
--- react-todo-list/src/components/TaskList.jsx
+++ react-todo-list/src/components/TaskList.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 
 const TaskList = ({ tasks, setTasks }) => {
+  const toggleStatus = (event) => {
+    let id = event.target.dataset.taskid;
+    let newStatus = !tasks[id].status;
+    let copyTasks = [...tasks];
+    copyTasks[id].status = newStatus;
+    // console.log(copyTasks);
+    setTasks(copyTasks);
+    // console.log(tasks);
+  };
+
   return (
     <section className='bg-pink-200 w-2/3 flex items-center justify-center px-20'>
-      <div className="container bg-slate-400 min-h-[40vh] p-4">
-        TaskList
-        {tasks.map((value, key) => (
-          <div key={key} className='flex justify-between'>
-            <div>{value.title}</div>
-            <div>{value.desc}</div>
-            <div>{value.status ? 'complete' : 'incomplete'}</div>
-          </div>
-        ))}
+      <div className="container bg-slate-400 max-h-[75vh] p-4">
+        <div>TaskList</div>
+        <div className='overflow-auto max-h-[65vh] bg-orange-300'>
+          {!tasks.length == 0 ?
+            (tasks.map((value, key) => (
+              <div key={key} className='flex justify-between'>
+                <div>{value.title}</div>
+                <div>{value.desc}</div>
+                <div key={`${key}-${value.status}1`}>{value.status ? 'complete' : 'incomplete'}</div>
+                <button data-taskid={key} key={`${key}-${value.status2}`} onClick={toggleStatus}>mark {!value.status ? 'complete' : 'incomplete'}</button>
+              </div>
+            )))
+            : 'No data to show'}
+          {/* {tasks.map((value, key) => (
+            <div key={key} className='flex justify-between'>
+              <div>{value.title}</div>
+              <div>{value.desc}</div>
+              <div>{value.status ? 'complete' : 'incomplete'}</div>
+            </div>
+          ))} */}
+        </div>
       </div>
     </section>
   )
